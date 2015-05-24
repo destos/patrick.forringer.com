34ffdab08bdf5ab6d2bdd73d4bd22cce7f6e95d0
Patrick Forringer <patrick@forringer.com>
2015-05-24 00:28:35 -0500

    fixing up post list page, getting tagging working
    monkey patching the template tag decorators too. :/

--- extensions/mezzanine_extensions.py
+++ extensions/mezzanine_extensions.py
@@ -2,7 +2,16 @@
 
 from jinja2.ext import Extension
 
+# patch shit
+from mezzanine import template
+prev_as_tag = template.Library.as_tag
+template.Library.as_tag = lambda lib, func: func
+
 from mezzanine.core.templatetags import mezzanine_tags
+from mezzanine.generic.templatetags.keyword_tags import keywords_for
+
+# kick it along
+template.Library.as_tag = prev_as_tag
 
 
 class MezzanineFilterExtension(Extension):
@@ -10,3 +19,4 @@ class MezzanineFilterExtension(Extension):
         super(MezzanineFilterExtension, self).__init__(environment)
         environment.globals["is_installed"] = mezzanine_tags.is_installed
         environment.filters["richtext_filters"] = mezzanine_tags.richtext_filters
+        environment.globals["keywords_for"] = keywords_for
